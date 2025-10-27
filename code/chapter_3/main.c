10784d3aee5d93a2b076c4ea7b57c88c73c6e7d5
harveyywang <harveyywang@tencent.com>
2025-10-27 17:24:12 +0800

    feat: 第 3 章代码示例

---
+++ code/chapter_3/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void mulstore(long,long,long*);
+
+
+int main(){
+    long d;
+    mulstore(2,3,&d);
+    printf("2 * 3 --> %ld\n",d);
+    return 0;
+}
+
+long mult2(long a,long b){
+    long s = a * b;
+    return s;
+}
